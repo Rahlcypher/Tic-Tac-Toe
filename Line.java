029cc76314df8ab1169a097a4df463a2114cb684
Rahlcypher <1monemailfake@gmail.com>
2024-02-27 18:37:56 +0100

    ce fichier est ce lui qui reference entite ligne dans notre jeu

---
+++ Line.java
@@ -0,0 +1,13 @@
+
+import java.awt.Component;
+public class Line{
+    public Component p1 ;
+    public Component p2 ;
+    public Component p3 ; 
+    Line(Component a ,Component b , Component c ){
+        this.p1  = a;
+        this.p2  = b;
+        this.p3  = c;
+    }
+}
+
